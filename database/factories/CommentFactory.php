9be68871ca7c82adeb2457af4ac9dd829082f763
arcen <arcen.dev@gmail.com>
2023-01-08 13:54:40 +0700

    database factory and seeder

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->safeEmail(),
+            'content' => $this->faker->text(mt_rand(50, 255)),
+            'blog_id' => mt_rand(1, 50),
+        ];
+    }
+}
