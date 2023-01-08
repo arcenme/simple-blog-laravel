9adb720529450b997e9ba7f038f1491bb3349450
arcen <arcen.dev@gmail.com>
2023-01-09 01:12:35 +0700

    add form new comment

--- resources/views/pages/landing_pages/blog/detail.blade.php
+++ resources/views/pages/landing_pages/blog/detail.blade.php
@@ -27,7 +27,32 @@
                             <h4>Comments</h4>
                         </div>
                         <div class="card-body">
-                            <ul class="list-unstyled list-unstyled-border list-unstyled-noborder comments-section"> </ul>
+                            <form class="row g-3">
+                                <div class="col-md-6 position-relative">
+                                    <label for="fullname" class="form-label">Fullname</label>
+                                    <input type="text" class="form-control" id="fullname" value="" required>
+                                    <div class="invalid-feedback validationFullname"> </div>
+                                </div>
+                                <div class="col-md-6 position-relative">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="text" class="form-control" id="email" value="" required>
+                                    <div class="invalid-feedback validationEmail"> </div>
+                                </div>
+
+                                <div class="col-12 position-relative mt-2">
+                                    <label for="comment" class="form-label ">Comment</label>
+                                    <textarea class="form-control" name="comment" rows="4" cols="50" required style="min-height: 150px"></textarea>
+                                    <div class="invalid-feedback validationComment"> Please provide a valid city.
+                                    </div>
+                                </div>
+                                <div class="col-12 my-3">
+                                    <button class="btn btn-primary btn-submit-comment" type="button">Submit comment</button>
+                                </div>
+                            </form>
+
+                            <div class="section mt-5">
+                                <ul class="list-unstyled list-unstyled-border list-unstyled-noborder comments-section"> </ul>
+                            </div>
 
                             <div class="d-flex justify-content-center">
                                 <button class="btn btn-success btn-sm btn-loadmore" style="display: none">Load more comments</button>
@@ -64,10 +89,6 @@
                 })
                 // display comment
                 $('.comments-section').empty().append(html)
-
-                // buttn loadmore
-                $('.btn-loadmore').css('display', comments.length > 0 && comments.length % 5 === 0 ? 'flex' : 'none')
-                $('.btn-loadmore').text('Load more comments')
             }
 
             // get comment data
@@ -84,9 +105,14 @@
                         $('.btn-loadmore').text('...loading')
                     },
                     success: function(res) {
+                        const commantLength = comments.length
+                        // add comment
                         comments = comments.concat(res.data)
-
+                        // render comment
                         renderComment()
+                        // buttn loadmore
+                        $('.btn-loadmore').css('display', comments.length > 0 && commantLength !== comments.length && comments.length % 5 === 0 ? 'flex' : 'none')
+                        $('.btn-loadmore').text('Load more comments')
                     },
                     error: function(err) {
                         $('.btn-loadmore').css('display', comments.length > 0 ? 'flex' : 'none')
